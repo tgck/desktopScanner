76b051e720c41a5b18f757e3fb9c0f019e982c47
tgck <taniguchiko@gmail.com>
2016-02-14 02:15:37 +0900

    finder項目(rubyCocoa/scriptingBridge) - cleaning

--- test/rbfitems/a.rb
+++ test/rbfitems/a.rb
@@ -9,36 +9,25 @@ include OSX
 OSX.require_framework 'ScriptingBridge'
 require 'b.rb'
 
-# 関数定義
-def list(files)
-	# dummy func
-	for i in files do
-		p = i.desktopPosition
-		puts sprintf("%.2f %.2f", p.x, p.y)
-	end
-end
-
 # メイン処理
 finder = SBApplication.applicationWithBundleIdentifier("com.apple.finder")
 
 files = finder.files
-names = files.arrayByApplyingSelector(:name) # array of NSMutableString
+names = files.arrayByApplyingSelector(:name)            # array of NSMutableString
 poss = files.arrayByApplyingSelector(:desktopPosition)  # array of NSConcreteValue
-paths = files.arrayByApplyingSelector(:URL)  # array of NSMutableString
+paths = files.arrayByApplyingSelector(:URL)             # array of NSMutableString
 
 posixPaths = []
 paths.each do |i| 
 	posixPaths.push(NSURL.URLWithString(i).path)
 end
 
-# list(files)
-
 targets = []
 for i in 0..files.length-1 do
-#while i < files.length
-	item = Item.new(names[i], poss[i].pointValue.x, poss[i].pointValue.y)
-	#puts sprintf("%s %s %s", names[i], poss[i].pointValue.x, poss[i].pointValue.y)
-	targets.push(item)
-	item.print
+	targets.push(Item.new(names[i], poss[i].pointValue.x, poss[i].pointValue.y))
 end
-#
+
+# 確認
+for i in targets
+	i.print
+end
