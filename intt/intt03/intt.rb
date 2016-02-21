bcc084e6332568e6d8ce31dea66f1bd6713d5104
tgck <taniguchiko@gmail.com>
2016-02-22 00:30:17 +0900

    ディスプレイサイズによる座標の正規化 - アニメーション

--- intt/intt03/intt.rb
+++ intt/intt03/intt.rb
@@ -14,6 +14,10 @@ OSX.require_framework 'ScriptingBridge'
 @screenStr = `osascript -e 'tell application "Finder" to get bounds of window of desktop'`.split().values_at(2,3)
 @screen = [@screenStr[0].to_f, @screenStr[1].to_f]
 
+# for Console Animation
+STEP = 20
+UNIT_VAL = 1.0/STEP
+
 # メソッドチェーンを実現するためのモンキーパッチング
 # カレントオブジェクト self については 以下を参照
 # http://qiita.com/ToruFukui/items/be29968da6dc9d125315
@@ -31,8 +35,44 @@ def getCGPoint()
 end
 
 loop do
+	# p = getCGPoint().normalizeXY(@screen)
+	# print( p[0], "      ", p[1], "\r")
+	# STDOUT.flush
+	# sleep(1)
+
 	p = getCGPoint().normalizeXY(@screen)
-	print( p[0], "      ", p[1], "\r")
+
+
+	where_x = (p[0] / UNIT_VAL).round
+	where_y = (p[1] / UNIT_VAL).round
+	
+	#print(where_x, ",", where_y, "\n")
+	#puts sprintf("%02d,%02d", where_x, where_y)
 	STDOUT.flush
-	sleep(1)
+
+	print("[")
+	for i in 1..STEP do 
+		if (i==where_x)
+			print("*")
+		else
+			print(" ")
+		end
+	end
+	print ('] [')
+	for i in 1..STEP do 
+		if (i==where_y)
+			print("*")
+		else
+			print(" ")
+		end
+	end
+	print("]", "\r")
+	STDOUT.flush
+	sleep(0.1)
 end
+
+# 1.0 を 20ステップなら、1ステップは0.05
+# 0.05 を UNIT_VAL と定義
+# X, Y それぞれの中に UNIT_VAL がいくつ含まれるかを算出
+# for i in 1..STEP でまわして、idx != UNIT_VAL なら スペースをprint
+#                           idx == UNIT_VAL なら * をprint
